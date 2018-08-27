8599ee4ee4b8a1638dd79d9818764ddf3de3a1f6
Piotr Rzeczkowski <piotr@rzeka.net>
2018-08-27 16:30:27 +0200

    Allow to pass custom arguments to builders

--- Tests/Menu/MenuContainerTest.php
+++ Tests/Menu/MenuContainerTest.php
@@ -4,6 +4,8 @@ namespace Rzeka\MenuBundle\Tests\Menu;
 use PHPUnit\Framework\TestCase;
 use Rzeka\Menu\MenuItem;
 use Rzeka\MenuBundle\Exception\InvalidMenuNameException;
+use Rzeka\MenuBundle\Exception\MenuBuilderArgumentsUnsupportedException;
+use Rzeka\MenuBundle\Menu\MenuBuilderArgsInterface;
 use Rzeka\MenuBundle\Menu\MenuBuilderInterface;
 use Rzeka\MenuBundle\Menu\MenuContainer;
 
@@ -79,5 +81,61 @@ class MenuContainerTest extends TestCase
 
         $this->expectException(InvalidMenuNameException::class);
         $container->getMenu('test');
+    }
+
+    public function testGetMenuWithArgs()
+    {
+        $uniqueId = 'test';
+        $args = [
+            'testArg'
+        ];
+        $name = 'test';
+        $menu = new MenuItem($name);
+
+        $builder = $this->createMock(MenuBuilderArgsInterface::class);
+        $builder
+            ->expects(static::once())
+            ->method('getName')
+            ->willReturn($name);
+
+        $builder
+            ->expects(static::once())
+            ->method('buildWithArgs')
+            ->with(...$args)
+            ->willReturn($menu);
+
+        $container = new MenuContainer();
+        $container->addBuilder($builder);
+
+        $result = $container->getMenuWithArgs($name, $uniqueId, ...$args);
+        static::assertEquals($menu, $result);
+
+        //shouldn't call builder
+        $container->getMenuWithArgs($name, $uniqueId, ...$args);
+    }
+
+    public function testGetNonExistentMenuWithArgs()
+    {
+        $container = new MenuContainer();
+
+        $this->expectException(InvalidMenuNameException::class);
+        $container->getMenuWithArgs('test', '', ...['test']);
+    }
+
+    public function testGetMenuWithArgsNoArgsInterface()
+    {
+        $name = 'test';
+
+        $builder = $this->createMock(MenuBuilderInterface::class);
+        $builder
+            ->expects(static::once())
+            ->method('getName')
+            ->willReturn($name);
+
+        $container = new MenuContainer();
+        $container->addBuilder($builder);
+
+        $this->expectException(MenuBuilderArgumentsUnsupportedException::class);
+        $container->getMenuWithArgs($name, 'test', ...['test']);
     }
 }
